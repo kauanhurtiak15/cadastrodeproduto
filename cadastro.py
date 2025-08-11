8572035102c865cd30a22e373a3d91372860ea47
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2025-08-11 14:13:32 +0000

    parte1 do projeto

---
+++ cadastro.py
@@ -0,0 +1,17 @@
+def formatar_nome(nome) :
+    return nome.strip().title()
+
+def cadastrar_produto():
+    nome = input("Digite o nome do produto: ")
+    preco = float(input("Digite o preço do produto: "))
+    categoria = input("Digite a categoria do produto: ")
+    return (formatar_nome(nome), preco, categoria)
+
+def salvar_produto(produto):
+    with open("produtos.txt", "a", encoding="ulf-a") as arquivo:
+        linha = f"(produto[0]);(produto[1]);(produto[2])\n"
+        arquivo.write(linha)
+
+def listar_produtos():
+    try:
+        with open("produtod.txt", "r", encoding="ulf-8")
